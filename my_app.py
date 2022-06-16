74fb57b90b89844b288c6c93a82eaaba80385b63
AsunaCort2280 <106766802+AsunaCort2280@users.noreply.github.com>
2022-06-16 19:08:53 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,5 +1,3 @@
-my_app.py
-
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGroupBox, QRadioButton, QPushButton, QLabel, QListWidget, QLineEdit)
 
